b573974922d55a736c0cd10183c8a74bd66a8d0c
devskope <pmskope@gmail.com>
2022-12-09 04:36:31 +0100

    feat-fixed_script_access_control

--- installer.sh
+++ installer.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo -e "############################################################"
 echo -e "#                        \033[31;1;4mshh\033[0m                               #"
 echo -e "#      Script to create script files with #!/bin/bash      #"
@@ -10,24 +9,23 @@ echo -e "#                     shh <script_name>                    #"
 echo -e "#             TODO: options, help, e.t.c                   #"
 echo -e "############################################################\n\n"
 
-if [ "$(id -u)" != "0" ]
-then
-	echo "Sorry, you are not root. Maybe run sudo ./installer.sh"
-	exit 1
+if [ "$(id -u)" != "0" ]; then
+  echo "Sorry, you are not root. Maybe run sudo ./installer.sh"
+  exit 1
 fi
 
 echo Installing to /usr/local/bin
 
-for i in {1..10}
-do
+for i in {1..10}; do
   printf "#"
   sleep 0.25
 done
 
-cp ./ssh /usr/local/bin 2>/dev/null
+# make runnable and copy
+chmod 777 ./shh
+cp ./shh /usr/local/bin 2>/dev/null
 
 printf " Done! ✅ \n\n"
 
-
-echo -e "USAGE:\t shh <script_name> <path>  \n\t ssh <script_name>"
+echo -e "USAGE:\t shh <script_name> <path>  \n\t shh <script_name>"
 tput bel 2>/dev/null
